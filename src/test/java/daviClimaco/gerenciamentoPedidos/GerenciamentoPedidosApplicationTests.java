7ed8bb7b2783ab0949620f78d0a947c6b522e19d
daviClimaco <davibomfimclimaco@gmail.com>
2026-03-22 20:57:29 -0300

    Iniciando Spring Initializer

---
+++ src/test/java/daviClimaco/gerenciamentoPedidos/GerenciamentoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package daviClimaco.gerenciamentoPedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
